341a06101183cd731868ff624fa84c675fe2e6bf
Olaitan <olaitanmaryakano@gmail.com>
2023-09-07 08:57:56 +0100

    Update time

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function updateDateTime() {
     const currentDayOfTheWeekElement = document.getElementById("currentDayOfTheWeek");
-    const currentUTCTimeElement = document.getElementById("currentUTCTime");
+   /* const currentUTCTimeElement = document.getElementById("currentUTCTime"); */
     const currentUTCTimeMillisElement = document.getElementById("currentUTCTimeMillis");
     const now = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'Africa/Lagos' };
     const currentDayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' });
-    const currentTime = now.toLocaleTimeString('en-US', {timeZone: 'Africa/Lagos' });
+   /* const currentTime = now.toLocaleTimeString('en-US', {timeZone: 'Africa/Lagos' }); */
     const currentMillis = now.getTime();
     currentDayOfTheWeekElement.textContent = ` ${currentDayOfWeek}`;
-    currentUTCTimeElement.textContent = ` ${currentTime}`;
+  /* currentUTCTimeElement.textContent = ` ${currentTime}`; */
     currentUTCTimeMillisElement.textContent = `${currentMillis}`;
 }
 
